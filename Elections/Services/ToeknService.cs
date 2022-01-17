ccc5aab1f0921e3ef59dbcb6b24e1d402367990f
rel <rel@mail.com>
2022-01-17 15:15:58 +0200

    Add ToeknService Service

---
+++ Elections/Services/ToeknService.cs
@@ -0,0 +1,18 @@
+﻿
+using Elections.Interfaces;
+using Elections.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elections.Services
+{
+    public class ToeknService : ITokenService
+    {
+        public string CreateToken(Voter voter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
