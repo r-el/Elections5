cd2547ebe201760c55f325105e8c6d61cce00c1a
rel <rel@mail.com>
2022-01-17 15:15:30 +0200

    Add ITokenService Interface

---
+++ Elections/Interfaces/ITokenService.cs
@@ -0,0 +1,13 @@
+﻿using Elections.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Elections.Interfaces
+{
+    interface ITokenService
+    {
+        string CreateToken(Voter voter);
+    }
+}
